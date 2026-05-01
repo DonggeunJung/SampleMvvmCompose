2886f85c935e49faa52bdb3e510dd5cbe58f7ffb
DONGGEUN JUNG <topsan72@gmail.com>
2026-05-01 16:47:55 -0400

    Use stateIn() in ViewModel

--- app/src/main/java/com/example/samplemvvmcompose/view/MainViewModel.kt
+++ app/src/main/java/com/example/samplemvvmcompose/view/MainViewModel.kt
@@ -1,36 +1,28 @@
 package com.example.samplemvvmcompose.view
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.samplemvvmcompose.domain.MyRepository
 import com.example.samplemvvmcompose.model.UserState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 @HiltViewModel
-class MainViewModel @Inject constructor (private val repository: MyRepository): ViewModel() {
-    private val _userState = MutableStateFlow<UserState>(UserState.Loading)
-    val userState: StateFlow<UserState> = _userState
+class MainViewModel @Inject constructor (private val repository: MyRepository): ViewModel() { //private val _userState = MutableStateFlow<UserState>(UserState.Loading)
 
-    init {
-        fetchUserProfile()
-    }
+    val userState: StateFlow<UserState> = repository.fetchUserProfile()
+        .onStart { UserState.Loading }
+        .catch { e -> UserState.Error(e.message ?: "Unknown error") }
+        .map { user -> UserState.Success(user) }
+        .stateIn(scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = UserState.Loading
+        )
 
-    fun fetchUserProfile() {
-        _userState.value = UserState.Loading
-        viewModelScope.launch {
-            repository.fetchUserProfile()
-                .catch { exception ->
-                    Log.e("MainViewModel", "Error - MyRepository.fetchUserProfile() : ${exception.message}")
-                    _userState.value = UserState.Error(exception.message.toString())
-                }.collect { user ->
-                    _userState.value = UserState.Success(user)
-                }
-        }
-    }
 }
