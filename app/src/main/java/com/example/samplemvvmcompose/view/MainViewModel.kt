205ef3c5f67d330d0fd0722401cd430a182509ae
DONGGEUN JUNG <topsan72@gmail.com>
2026-05-01 17:53:01 -0400

    Remove useless import library

--- app/src/main/java/com/example/samplemvvmcompose/view/MainViewModel.kt
+++ app/src/main/java/com/example/samplemvvmcompose/view/MainViewModel.kt
@@ -10,7 +10,6 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 
 @HiltViewModel
